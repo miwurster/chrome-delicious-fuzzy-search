96627eb8c88e0b177eb780c29b6391f8ecc4918d
miwurster <miwurster@googlemail.com>
2013-11-09 13:42:43 +0100

    Major refactoring part 6

--- app/scripts/controller/optionController.js
+++ app/scripts/controller/optionController.js
@@ -2,22 +2,36 @@
 
 angular.module('delicious-fuzzy-search')
     .controller('OptionsController', [ '$scope', 'DataStore', function ($scope, DataStore) {
-        $scope.data = {};
-        $scope.data.username = '';
-        $scope.data.password = '';
+        $scope.cache = {};
+
+        $scope.username = '';
+        $scope.password = '';
 
         $scope.init = function () {
-            $scope.data = DataStore.load($scope.CONST.NAMESPACE_OPTIONS);
+            $scope.cache = DataStore.load($scope.CONST.NAMESPACE_OPTIONS);
+            if (!$scope.isCacheEmpty()) {
+                $scope.username = $scope.cache.username;
+                $scope.password = $scope.cache.password;
+            }
         };
 
         $scope.save = function () {
-            DataStore.save($scope.CONST.NAMESPACE_OPTIONS, $scope.data);
+            $scope.cache.username = $scope.username;
+            $scope.cache.password = $scope.password;
+            DataStore.save($scope.CONST.NAMESPACE_OPTIONS, $scope.cache);
         };
 
         $scope.resetCredentials = function () {
-            $scope.data.username = '';
-            $scope.data.password = '';
+            $scope.cache = {};
+            $scope.username = '';
+            $scope.password = '';
             $scope.save();
+        };
+
+        $scope.isCacheEmpty = function () {
+            return _.isEmpty($scope.cache)
+                || _.isEmpty($scope.cache.username)
+                || _.isEmpty($scope.cache.password);
         };
 
         $scope.init();
