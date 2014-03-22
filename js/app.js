b5cec65d8277c95fa9b6d909c85b08b9fb7cfadc
miwurster <miwurster@googlemail.com>
2014-03-22 18:12:03 +0100

    Implemented work flow to request an access token for the Delicious API

--- js/app.js
+++ js/app.js
@@ -1,14 +1,23 @@
 'use strict';
 
-angular.module('delicious-fuzzy-search', ['ui.router', 'miwurster.utils']);
+angular.module('delicious-fuzzy-search', ['ui.router', 'miwurster.utils', 'miwurster.alert']);
 
 angular.module('delicious-fuzzy-search')
 
-    .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    .constant('consts', {
+        APP_KEY: '9bb4def552ddd6ec30f5427f2f29b162',
+        APP_KEY_SECRET: '66b65558747ccc5c57d765ff2e1c9635',
+        REDIRECT_URL: 'http://localhost/dfs-page'
+    })
+
+    .config(['$locationProvider', '$stateProvider', '$urlRouterProvider', function ($locationProvider, $stateProvider, $urlRouterProvider) {
+        $locationProvider.html5Mode(false);
+        $locationProvider.hashPrefix('!');
         $stateProvider
             .state('site', {
                 url: '/site',
                 abstract: true,
+                controller: 'site',
                 templateUrl: 'index.tpl.html'
             })
             .state('site.about', {
@@ -18,6 +27,62 @@ angular.module('delicious-fuzzy-search')
                         templateUrl: 'about.tpl.html'
                     }
                 }
+            })
+            .state('site.delicious', {
+                url: '/delicious',
+                views: {
+                    'content': {
+                        controller: 'delicious',
+                        templateUrl: 'delicious.tpl.html'
+                    }
+                },
+                resolve: {
+                    oauthCode: function ($location) {
+                        var oauth = $location.absUrl().match(/\/\?code=([a-zA-Z0-9]+)/);
+                        if (oauth !== null) {
+                            return oauth[1];
+                        }
+                        return undefined;
+                    }
+                }
             });
         $urlRouterProvider.otherwise('/site/about');
-    }]);
+    }])
+
+    .run(['$rootScope', '$location', '$state', function ($rootScope, $location, $state) {
+        $rootScope.$on('$stateChangeStart', function (event, nextState, newStateParams, lastState, lastStateParams) {
+            var redirected = $location.absUrl().match(/\/\?code=([a-zA-Z0-9]+)/);
+            if (redirected !== null && nextState.name === 'site.about') {
+                event.preventDefault();
+                $state.go('site.delicious');
+            }
+        });
+    }])
+
+    .controller('site', ['$scope', 'consts', function ($scope, consts) {
+        $scope.requestUrl = 'https://delicious.com/auth/authorize'
+            + '?client_id=' + consts.APP_KEY
+            + '&redirect_uri=' + consts.REDIRECT_URL;
+    }])
+
+    .controller('delicious', ['$scope', '$log', '$http', '$state', 'consts', 'oauthCode',
+        function ($scope, $log, $http, $state, consts, oauthCode) {
+            if (!oauthCode) {
+                $state.go('site.about');
+            }
+            var oauthUrl = 'https://avosapi.delicious.com/api/v1/oauth/token'
+                + '?client_id=' + consts.APP_KEY
+                + '&client_secret=' + consts.APP_KEY_SECRET
+                + '&grant_type=code&code=' + oauthCode;
+            $http.post(oauthUrl).
+                success(function (data) {
+                    var status = data.status;
+                    var accessToken = data.access_token;
+                    if (status === 'success') {
+                        $scope.accessToken = accessToken;
+                    } else {
+                        $scope.error = { code: status };
+                        $log.error('Could not request access token:', status);
+                    }
+                });
+        }]);
