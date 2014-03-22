a94ac12407655631d7dcfbc521a4af24a6505bd4
miwurster <miwurster@googlemail.com>
2014-03-22 15:29:24 +0100

    Split html files into required template files

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
 'use strict';
 
-angular.module('delicious-fuzzy-search', ['miwurster.utils']);
+angular.module('delicious-fuzzy-search', ['ui.router', 'miwurster.utils']);
 
-angular.module('delicious-fuzzy-search');
+angular.module('delicious-fuzzy-search')
+
+    .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('site', {
+                url: '/site',
+                abstract: true,
+                templateUrl: 'index.tpl.html'
+            })
+            .state('site.about', {
+                url: '/about',
+                views: {
+                    'content': {
+                        templateUrl: 'about.tpl.html'
+                    }
+                }
+            });
+        $urlRouterProvider.otherwise('/site/about');
+    }]);
