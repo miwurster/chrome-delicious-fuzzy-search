d337f46fd1fdc9e594f791b1f4023de510389f7f
miwurster <miwurster@googlemail.com>
2014-03-23 14:01:46 +0100

    One step further

--- app/js/app.js
+++ app/js/app.js
@@ -14,7 +14,8 @@ angular.module('delicious-fuzzy-search')
     })
 
     .config(['$httpProvider', function ($httpProvider) {
-        $httpProvider.defaults.withCredentials = true;
+        //$httpProvider.defaults.withCredentials = true;
+        /**
         $httpProvider.defaults.cache = true;
         $httpProvider.defaults.transformResponse = function (data) {
             var posts = [];
@@ -31,21 +32,46 @@ angular.module('delicious-fuzzy-search')
             });
             return posts;
         }
+        */
     }])
 
-    .controller('options', ['$scope', '$http', 'consts', function ($scope, $http, consts) {
+    .controller('options', ['$scope', '$log', '$http', 'consts', function ($scope, $log, $http, consts) {
         $scope.redirectUrl = 'chrome-extension://' + consts.EXTENSION_ID + '/options.html';
-        $scope.redirectUrl = 'http://miwurster.github.io/chrome-delicious-fuzzy-search';
+        // $scope.redirectUrl = 'http://miwurster.github.io/chrome-delicious-fuzzy-search';
         $scope.clientId = consts.APP_KEY;
         $scope.clientSecret = consts.APP_KEY_SECRET;
 
-        $scope.accessToken = '7548957-672264c6df7fac333d0eede86d233599';
+        var url = 'https://avosapi.delicious.com/api/v1/oauth/token'
+            + '?client_id=' + consts.APP_KEY
+            + '&client_secret=' + consts.APP_KEY_SECRET
+            // + '&grant_type=code&code=' + oauthCode;
+            + '&grant_type=credentials&username=miwurster&password=ttcyx18Mi';
 
-        $http.defaults.headers.common.Authorization = 'Bearer ' + $scope.accessToken;
-        $http.get('https://api.del.icio.us/v1/posts/all').success(
-            function (data) {
-                console.log(JSON.stringify(data));
-            }
-        );
-        $http.defaults.headers.common.Authorization = '';
+        $http.defaults.headers.common.Accept = 'application/json';
+        $http.post(url).
+            success(function (data) {
+                var status = data.status;
+                var accessToken = data.access_token;
+                if (status === 'success') {
+                    $scope.accessToken = accessToken;
+                } else {
+                    $scope.error = { code: status };
+                    $log.error('Could not request access token:', status);
+                }
+            }).
+            error(function (data, status, headers, config) {
+                $log.error('Error getting access token:', status);
+                $log.debug(JSON.stringify(config));
+            });
+
+
+//        $scope.accessToken = '7548957-672264c6df7fac333d0eede86d233599';
+//
+//        $http.defaults.headers.common.Authorization = 'Bearer ' + $scope.accessToken;
+//        $http.get('https://api.del.icio.us/v1/posts/all').success(
+//            function (data) {
+//                console.log(JSON.stringify(data));
+//            }
+//        );
+//        $http.defaults.headers.common.Authorization = '';
     }]);
