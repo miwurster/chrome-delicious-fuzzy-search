d521cca249cf0a57d5aa1e1e1ceee8c955202de2
miwurster <miwurster@googlemail.com>
2015-03-14 13:29:43 +0100

    Bootstrapped

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,9 +1,31 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function (details) {
-  console.log('previousVersion', details.previousVersion);
+function focusOrCreateTab(url) {
+  chrome.windows.getAll({'populate': true}, function (windows) {
+    var existingTab;
+    for (var i in windows) {
+      if (windows.hasOwnProperty(i)) {
+        var tabs = windows[i].tabs;
+        for (var j in tabs) {
+          if (tabs.hasOwnProperty(j)) {
+            var tab = tabs[j];
+            if (tab.url === url) {
+              existingTab = tab;
+              break;
+            }
+          }
+        }
+      }
+    }
+    if (existingTab) {
+      chrome.tabs.update(existingTab.id, {'selected': true});
+    } else {
+      chrome.tabs.create({'url': url, 'selected': true});
+    }
+  });
+}
+
+chrome.browserAction.onClicked.addListener(function () {
+  var url = chrome.extension.getURL('search.html');
+  focusOrCreateTab(url);
 });
-
-chrome.browserAction.setBadgeText({text: '\'Allo'});
-
-console.log('\'Allo \'Allo! Event Page for Browser Action');
