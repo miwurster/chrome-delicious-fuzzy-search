c8fed733642a7289f44aea3537154be4f88c472f
miwurster <miwurster@googlemail.com>
2015-03-15 15:33:10 +0100

    Clean up

--- Gruntfile.js
+++ Gruntfile.js
@@ -96,7 +96,8 @@ module.exports = function (grunt) {
           dot: true,
           src: [
             '<%= config.dist %>',
-            '.tmp'
+            '.tmp',
+            'package'
           ]
         }]
       }
